5fc94a813050366d0ef300e3400c0d8704061182
jsrevathy <revathy.researchscholar@gmail.com>
2022-05-16 07:57:42 +0530

    Commited the Repo

---
+++ java/rna-transcription/src/main/java/RnaTranscription.java
@@ -0,0 +1,25 @@
+class RnaTranscription {
+    String transcribe(String dnaStrand) {
+        StringBuilder rna = new StringBuilder();
+        
+        for ( int i = 0; i < dnaStrand.length(); i++) {
+
+    		if (dnaStrand.charAt(i) == 'G') 
+            {
+            	rna.append('C');
+            }
+            else if (dnaStrand.charAt(i) == 'C') {
+                rna.append('G');
+                }
+            else if (dnaStrand.charAt(i) == 'T') {
+                rna.append('A');
+                }
+            else if (dnaStrand.charAt(i) == 'A') {
+                rna.append('U');}
+    		else {
+    			throw new IllegalArgumentException("Invalid input");
+    		}
+    	}      
+       	return rna.toString();
+    }
+}
